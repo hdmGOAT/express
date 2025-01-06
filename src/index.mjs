9add051459a3f3fbfe5b5a9b091f9437ffe6dd51
Hans Matthew Del Mundo <hdmGOAT@users.noreply.github.com>
2025-01-06 14:40:42 +0800

    progress

--- src/index.mjs
+++ src/index.mjs
@@ -4,6 +4,14 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+app.get("/", (request, response) => {
+  response.status(201).send({ msg: "hello" });
+});
+
+app.get("/cool", (request, response) => {
+  response.send(request.ip);
+});
+
 app.listen(PORT, () => {
-  console.log("Running on Port ${PORT}");
+  console.log(`Running on Port ${PORT}`);
 });
