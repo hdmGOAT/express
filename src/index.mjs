e6bcc1e41dca067701c4d7e29673c5c64d14969a
Hans Matthew Del Mundo <hdmGOAT@users.noreply.github.com>
2025-01-06 14:52:09 +0800

    okay i get GET

--- src/index.mjs
+++ src/index.mjs
@@ -8,8 +8,20 @@ app.get("/", (request, response) => {
   response.status(201).send({ msg: "hello" });
 });
 
-app.get("/cool", (request, response) => {
-  response.send(request.ip);
+app.get("/api/users", (request, response) => {
+  response.send([
+    { id: 1, username: "hanscoolguy3000", displayName: "hans" },
+    { id: 2, username: "irairairiariari", displayName: "ira" },
+    { id: 3, username: "aasdfadawe", displayName: "wow" },
+  ]);
+});
+
+app.get("/api/products", (request, response) => {
+  response.send([
+    { id: 1, itemName: "gun", price: "100" },
+    { id: 2, itemName: "bomb", price: "50" },
+    { id: 3, itemName: "idk", price: "10" },
+  ]);
 });
 
 app.listen(PORT, () => {
