d3356b5945a9e7baf63148f440221390dc813f9b
Hans Matthew Del Mundo <hdmGOAT@users.noreply.github.com>
2025-01-09 10:42:51 +0800

    sessions

--- src/index.mjs
+++ src/index.mjs
@@ -1,11 +1,23 @@
 import express, { request, response } from "express";
 import routes from "./routes/index.mjs";
 import cookieParser from "cookie-parser";
+import session from "express-session";
 
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser("helloWorld"));
+app.use(
+  session({
+    secret: "this is a temporary secret",
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+      maxAge: 60_000 * 60,
+    },
+  })
+);
+
 app.use(routes);
 
 const PORT = process.env.PORT || 3000;
@@ -15,6 +27,8 @@ app.listen(PORT, () => {
 });
 
 app.get("/", (request, response) => {
+  console.log(request.session);
+  console.log(request.session.id);
   response.cookie("hello", "world", { maxAge: 60_000 * 60 * 2, signed: true });
   response.status(201).send({ msg: "hello" });
 });
