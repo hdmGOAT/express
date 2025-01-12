f75dd4e0c2285c2b53003be543a7f503c2e9958b
Hans Matthew Del Mundo <hdmGOAT@users.noreply.github.com>
2025-01-12 20:40:23 +0800

    integrated databse into local-strategy

--- src/strategies/local-strategy.mjs
+++ src/strategies/local-strategy.mjs
@@ -1,14 +1,14 @@
 import passport from "passport";
 import { Strategy } from "passport-local";
-import { mockUsers } from "../utils/constants.mjs";
+import { User } from "../mongoose/schemas/user.mjs";
 
 passport.serializeUser((user, done) => {
   done(null, user.id);
 });
 
-passport.deserializeUser((id, done) => {
+passport.deserializeUser(async (id, done) => {
   try {
-    const findUser = mockUsers.find((user) => user.id === id);
+    const findUser = await User.findById(id);
     if (!findUser) throw new Error("User not found");
     done(null, findUser);
   } catch (err) {
@@ -17,11 +17,11 @@ passport.deserializeUser((id, done) => {
 });
 
 export default passport.use(
-  new Strategy((username, password, done) => {
+  new Strategy(async (username, password, done) => {
     console.log("Username: ", username);
     console.log("Password: ", password);
     try {
-      const findUser = mockUsers.find((user) => user.username === username);
+      const findUser = await User.findOne({ username });
 
       if (!findUser) throw new Error("user not found");
       if (findUser.password !== password) throw new Error("incorrect password");
