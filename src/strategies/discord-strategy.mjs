60d487182a235ddee7c9d30ea64330f31c58c583
Hans Matthew Del Mundo <hdmGOAT@users.noreply.github.com>
2025-01-13 21:54:40 +0800

    discord

--- src/strategies/discord-strategy.mjs
+++ src/strategies/discord-strategy.mjs
@@ -1,2 +1,17 @@
 import passport from "passport";
 import { Strategy } from "passport-discord";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+passport.use(
+  new Strategy(
+    {
+      clientID: process.env.CLIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET,
+      callbackURL: process.env.CLIENT_REDIRECT,
+      scope: ["identify"],
+    },
+    () => {}
+  )
+);
