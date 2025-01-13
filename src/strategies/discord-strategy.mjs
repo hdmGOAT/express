4e71745673b7059e0e0ffc12665318cc50bd1860
Hans Matthew Del Mundo <hdmGOAT@users.noreply.github.com>
2025-01-13 22:17:00 +0800

    readying to save to database

--- src/strategies/discord-strategy.mjs
+++ src/strategies/discord-strategy.mjs
@@ -1,6 +1,7 @@
 import passport from "passport";
 import { Strategy } from "passport-discord";
 import dotenv from "dotenv";
+import { DiscordUser } from "../mongoose/schemas/discord-user.mjs";
 
 dotenv.config();
 
@@ -12,8 +13,16 @@ export default passport.use(
       callbackURL: process.env.CLIENT_REDIRECT,
       scope: ["identify"],
     },
-    (accessToken, refreshToken, profile, done) => {
-      done();
+    async (accessToken, refreshToken, profile, done) => {
+      const findUser = await DiscordUser.findOne({ discordId: profile.id });
+      if (!findUser) {
+        const newUser = new DiscordUser({
+          username: profile.username,
+          discordId: profile.id,
+        });
+        const newSavedUser = await newUser.save();
+        done(null, newSavedUser);
+      }
     }
   )
 );
