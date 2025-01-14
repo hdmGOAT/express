91eaa9a33ab05725fbf98ba0cd13d602a2f76831
Hans Matthew Del Mundo <hdmGOAT@users.noreply.github.com>
2025-01-14 22:57:52 +0800

    added on assertions

--- src/__tests__/users.test.js
+++ src/__tests__/users.test.js
@@ -21,5 +21,10 @@ describe("get users", () => {
   it("should call sendStatus with 404 when user not found", () => {
     const copyMockRequest = { ...mockRequest, findUserIndex: 100 };
     getUserByIdHandler(copyMockRequest, mockResponse);
+    expect(mockResponse.sendStatus).toHaveBeenCalled();
+    expect(mockResponse.sendStatus).toHaveBeenCalledWith(404);
+    expect(mockResponse.sendStatus).toHaveBeenCalledTimes(1);
+    expect(mockResponse.send).not.toHaveBeenCalled();
+    
   });
 });
