eb6f92d9134a3e6c944b382b8dffa8efbb0282ca
Hans Matthew Del Mundo <hdmGOAT@users.noreply.github.com>
2025-01-14 22:55:39 +0800

    new mock equest setup

--- src/__tests__/users.test.js
+++ src/__tests__/users.test.js
@@ -15,5 +15,11 @@ describe("get users", () => {
     getUserByIdHandler(mockRequest, mockResponse);
     expect(mockResponse.send).toHaveBeenCalled();
     expect(mockResponse.send).toHaveBeenCalledWith(mockUsers[1]);
+    expect(mockResponse.send).toHaveBeenCalledTimes(1);
+  });
+
+  it("should call sendStatus with 404 when user not found", () => {
+    const copyMockRequest = { ...mockRequest, findUserIndex: 100 };
+    getUserByIdHandler(copyMockRequest, mockResponse);
   });
 });
