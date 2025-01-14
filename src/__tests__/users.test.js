5196024c70fa26b9f5e2475747644c0f43f6a56c
Hans Matthew Del Mundo <hdmGOAT@users.noreply.github.com>
2025-01-14 10:50:18 +0800

    adding more tests

--- src/__tests__/users.test.js
+++ src/__tests__/users.test.js
@@ -1,4 +1,5 @@
 import { getUserByIdHandler } from "../handlers/users.mjs";
+import { mockUsers } from "../utils/constants.mjs";
 
 const mockRequest = {
   findUserIndex: 1,
@@ -13,5 +14,6 @@ describe("get users", () => {
   it("should get user by id", () => {
     getUserByIdHandler(mockRequest, mockResponse);
     expect(mockResponse.send).toHaveBeenCalled();
+    expect(mockResponse.send).toHaveBeenCalledWith(mockUsers[1]);
   });
 });
